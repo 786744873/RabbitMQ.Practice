3261801edc49fbfc34b045e8f81b4d893a00f4c5
玉廷 王 <786744873@qq.com>
2018-05-23 13:07:08 +0800

    提交博客Demo

---
+++ PublishDemo/Program.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Text;
+using RabbitMQ.Client;
+using RabbitMQ.Client.Events;
+
+namespace PublishDemo
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            String queueName = "wytQueue";
+            String exchangeName = "wytExchange";
+            String routeKey = "wytRouteKey";
+            String message = "Hello World!";
+
+            ConnectionFactory factory = new ConnectionFactory();
+            factory.HostName = "localhost";
+            factory.Port = 5672;
+            factory.VirtualHost = "/wyt";
+            factory.UserName = "wyt";
+            factory.Password = "wyt";
+
+            using (IConnection connection=factory.CreateConnection())
+            {
+                using (IModel channel=connection.CreateModel())
+                {
+                    //声明交换机（名称：log，类型：fanout（扇出））
+                    channel.ExchangeDeclare(exchange: exchangeName, type: "direct");
+
+                    Byte[] body = Encoding.UTF8.GetBytes(message);
+
+                    //消息推送
+                    channel.BasicPublish(exchange: exchangeName, routingKey: routeKey, body: body);
+
+                    Console.WriteLine(" [x] Sent {0}", message);
+                }
+            }
+
+            Console.WriteLine(" Press [enter] to exit.");
+            Console.ReadLine();
+        }
+    }
+}
---
+++ SubscribeDemo/Program.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Text;
+using RabbitMQ.Client;
+using RabbitMQ.Client.Events;
+
+namespace SubscribeDemo
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            String queueName = "wytQueue";
+            String exchangeName = "wytExchange";
+            String routeKeyName = "wytRouteKey";
+
+            ConnectionFactory factory = new ConnectionFactory();
+            factory.HostName = "tcp://server.natappfree.cc:32910";
+            factory.Port = 5672;
+            factory.VirtualHost = "/wyt";
+            factory.UserName = "wyt";
+            factory.Password = "wyt";
+
+            using (IConnection connection=factory.CreateConnection())
+            {
+                using (IModel channel=connection.CreateModel())
+                {
+                    //声明扇形交换机
+                    channel.ExchangeDeclare(exchange: exchangeName, type: "direct");
+
+                    channel.QueueDeclare(queue: queueName, durable: false, exclusive: false, autoDelete: false, arguments: null);
+
+                    channel.QueueBind(queue: queueName, exchange: exchangeName, routingKey: routeKeyName, arguments: null);
+
+                    EventingBasicConsumer consumer = new EventingBasicConsumer(channel);
+                    consumer.Received += (model, ea) =>
+                    {
+                        Byte[] body = ea.Body;
+                        String message = Encoding.UTF8.GetString(body);
+                        Console.WriteLine(" [x] {0}", message);
+                    };
+
+                    channel.BasicConsume(queue: queueName, autoAck: true, consumer: consumer);
+
+                    Console.WriteLine(" Press [enter] to exit.");
+                    Console.ReadLine();
+
+                }
+            }
+        }
+    }
+}
